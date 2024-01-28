f59c7916a98089963bfdf1088c1b12ae35362e3d
Mark Erikson <mark@isquaredsoftware.com>
2024-01-28 13:25:33 -0500

    Add notifications slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,6 +3,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '@/features/auth/authSlice'
 import postsReducer from '@/features/posts/postsSlice'
 import usersReducer from '@/features/users/usersSlice'
+import notificationsReducer from '@/features/notifications/notificationsSlice'
 
 import { listenerMiddleware } from './listenerMiddleware'
 
@@ -11,6 +12,7 @@ export const store = configureStore({
     auth: authReducer,
     posts: postsReducer,
     users: usersReducer,
+    notifications: notificationsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(listenerMiddleware.middleware),
 })
