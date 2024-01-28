5d379f92f9b4645be7871d3f5d2d89d82984afb1
Mark Erikson <mark@isquaredsoftware.com>
2024-01-28 13:49:59 -0500

    Add a memoized `selectPostsByUser` selector

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { client } from '@/api/client'
 
 import type { RootState } from '@/app/store'
@@ -113,6 +113,11 @@ export default postsSlice.reducer
 export const selectAllPosts = (state: RootState) => state.posts.posts
 
 export const selectPostById = (state: RootState, postId: string) => state.posts.posts.find((post) => post.id === postId)
+
+export const selectPostsByUser = createSelector(
+  [selectAllPosts, (state: RootState, userId: string) => userId],
+  (posts, userId) => posts.filter((post) => post.user === userId),
+)
 
 export const addPostsListeners = (startListening: AppStartListening) => {
   startListening({
