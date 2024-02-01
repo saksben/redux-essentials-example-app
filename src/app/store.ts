2be226579c7cd0dc4e61e28af8f016cb15ec497c
Mark Erikson <mark@isquaredsoftware.com>
2024-01-31 21:10:21 -0500

    Add initial API slice with getPosts query

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,6 +4,7 @@ import authReducer from '@/features/auth/authSlice'
 import postsReducer from '@/features/posts/postsSlice'
 import usersReducer from '@/features/users/usersSlice'
 import notificationsReducer from '@/features/notifications/notificationsSlice'
+import { apiSlice } from '@/features/api/apiSlice'
 
 import { listenerMiddleware } from './listenerMiddleware'
 
@@ -13,8 +14,10 @@ export const store = configureStore({
     posts: postsReducer,
     users: usersReducer,
     notifications: notificationsReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(listenerMiddleware.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware, apiSlice.middleware),
 })
 
 // Infer the type of `store`
