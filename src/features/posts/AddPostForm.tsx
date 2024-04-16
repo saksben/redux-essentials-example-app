769d389617246c6778626de7e25ab5da399e676f
Mark Erikson <mark@isquaredsoftware.com>
2024-04-16 13:56:10 -0400

    Dispatch postAdded when "Save Post" is clicked

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { useAppDispatch } from '@/app/hooks'
+
+import { postAdded } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+
+  const dispatch = useAppDispatch()
 
   const onTitleChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value)
@@ -12,26 +19,32 @@ export const AddPostForm = () => {
     setContent(e.target.value)
   }
 
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content,
+        }),
+      )
+
+      setTitle('')
+      setContent('')
+    }
+  }
+
   return (
     <section>
       <h2>Add a New Post</h2>
       <form>
         <label htmlFor="postTitle">Post Title:</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          value={title}
-          onChange={onTitleChanged}
-        />
+        <input type="text" id="postTitle" name="postTitle" value={title} onChange={onTitleChanged} />
         <label htmlFor="postContent">Content:</label>
-        <textarea
-          id="postContent"
-          name="postContent"
-          value={content}
-          onChange={onContentChanged}
-        />
-        <button type="button">Save Post</button>
+        <textarea id="postContent" name="postContent" value={content} onChange={onContentChanged} />
+        <button type="button" onClick={onSavePostClicked}>
+          Save Post
+        </button>
       </form>
     </section>
   )
