f15d3cde3f358e5a5d96abaded47c46e20f118e6
Mark Erikson <mark@isquaredsoftware.com>
2024-05-12 13:19:16 -0400

    Add posts slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import postsReducer from '@/features/posts/postsSlice'
+
 export const store = configureStore({
   // Pass in the root reducer setup as the `reducer` argument
   reducer: {
-    // An example slice reducer function that returns a fixed state value
-    value: (state: number = 123) => state,
+    posts: postsReducer,
   },
 })
 
